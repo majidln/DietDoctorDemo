6d07f32453ccfc98e27334c50f74b5082e4f2023
majidln <majidlotfinia12@gmail.com>
2021-02-05 02:27:24 +0330

    config babel module resolver

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,24 @@
 module.exports = {
   root: true,
   extends: '@react-native-community',
+  settings: {
+    "import/resolver": {
+      "node": {
+        "extensions": [
+          ".js",
+          ".android.js",
+          ".ios.js"
+        ]
+      },
+      "babel-module": {
+        "@src": "./src",
+        "@screens": "./src/screens",
+        "@navigation": "./src/navigation",
+        "@atomic-components": "./src/components/atomic",
+        "@common-components": "./src/components/common",
+        "@screen-components": "./src/components/screen",
+        
+      }
+    }
+  }
 };
--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,19 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      require.resolve('babel-plugin-module-resolver'),
+      {
+        root: ['./src/'],
+        alias: {
+          '@src': './src',
+          '@screens': './src/screens',
+          '@navigation': './src/navigation',
+          '@atomic-components': './src/components/atomic',
+          '@common-components': './src/components/common',
+          '@screen-components': './src/components/screen',
+        },
+      },
+    ],
+  ],
 };
